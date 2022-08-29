deab8b1ab5a62081beaad56f640ee92236bc6f6d
Diego Izquierdo <izquierdodiegom@gmail.com>
2022-08-29 19:00:24 -0300

    terminado sin conexion a bd

--- familiares_app/models.py
+++ familiares_app/models.py
@@ -1,3 +1,11 @@
+from pyexpat import model
 from django.db import models
 
 # Create your models here.
+
+class Familiar(models.Model):
+    
+    nombre = models.CharField(max_length=40)
+    edad = models.IntegerField()
+    f_nac = models.DateTimeField()
+    
--- familiares_app/views.py
+++ familiares_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from .models import Familiar
+
 # Create your views here.
+
+def home(req):
+    
+    familiares = Familiar.objects.all()
+    
+    return render(req, "index.html", {"famillares":familiares})
+
